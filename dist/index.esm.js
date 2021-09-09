591f3f91e8d6aac480b5a32049f643a680223015
RuntimeErrorX <kholder2@gmail.com>
2021-09-08 22:58:16 -0400

    Menu updates

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -3844,10 +3844,10 @@ var links = [
         label: 'More',
         icon: 'MoreIcon',
         items: [
-            {
-                label: 'Contact',
-                href: 'https://docs.venetianxcapital.finance/contact-us',
-            },
+            // {
+            //   label: 'Contact',
+            //   href: 'https://docs.venetianxcapital.finance/contact-us',
+            // },
             // {
             //   label: t('Voting'),
             //   href: '/voting',
@@ -3878,62 +3878,62 @@ var socials = [
         items: [
             {
                 label: "English",
-                href: "https://t.me/pancakeswap",
+                href: "https://t.me/VenetianXCapital",
             },
-            {
-                label: "Bahasa Indonesia",
-                href: "https://t.me/PancakeSwapIndonesia",
-            },
-            {
-                label: "中文",
-                href: "https://t.me/PancakeSwap_CN",
-            },
-            {
-                label: "Tiếng Việt",
-                href: "https://t.me/PancakeSwapVN",
-            },
-            {
-                label: "Italiano",
-                href: "https://t.me/pancakeswap_ita",
-            },
-            {
-                label: "русский",
-                href: "https://t.me/pancakeswap_ru",
-            },
-            {
-                label: "Türkiye",
-                href: "https://t.me/pancakeswapturkiye",
-            },
-            {
-                label: "Português",
-                href: "https://t.me/PancakeSwapPortuguese",
-            },
-            {
-                label: "Español",
-                href: "https://t.me/PancakeswapEs",
-            },
-            {
-                label: "日本語",
-                href: "https://t.me/pancakeswapjp",
-            },
-            {
-                label: "Français",
-                href: "https://t.me/pancakeswapfr",
-            },
-            {
-                label: "Announcements",
-                href: "https://t.me/PancakeSwapAnn",
-            },
-            {
-                label: "Whale Alert",
-                href: "https://t.me/PancakeSwapWhales",
-            },
+            // {
+            //   label: "Bahasa Indonesia",
+            //   href: "https://t.me/PancakeSwapIndonesia",
+            // },
+            // {
+            //   label: "中文",
+            //   href: "https://t.me/PancakeSwap_CN",
+            // },
+            // {
+            //   label: "Tiếng Việt",
+            //   href: "https://t.me/PancakeSwapVN",
+            // },
+            // {
+            //   label: "Italiano",
+            //   href: "https://t.me/pancakeswap_ita",
+            // },
+            // {
+            //   label: "русский",
+            //   href: "https://t.me/pancakeswap_ru",
+            // },
+            // {
+            //   label: "Türkiye",
+            //   href: "https://t.me/pancakeswapturkiye",
+            // },
+            // {
+            //   label: "Português",
+            //   href: "https://t.me/PancakeSwapPortuguese",
+            // },
+            // {
+            //   label: "Español",
+            //   href: "https://t.me/PancakeswapEs",
+            // },
+            // {
+            //   label: "日本語",
+            //   href: "https://t.me/pancakeswapjp",
+            // },
+            // {
+            //   label: "Français",
+            //   href: "https://t.me/pancakeswapfr",
+            // },
+            // {
+            //   label: "Announcements",
+            //   href: "https://t.me/PancakeSwapAnn",
+            // },
+            // {
+            //   label: "Whale Alert",
+            //   href: "https://t.me/PancakeSwapWhales",
+            // },
         ],
     },
     {
         label: "Twitter",
         icon: "TwitterIcon",
-        href: "https://twitter.com/pancakeswap",
+        href: "https://twitter.com/capitalvenetian",
     },
 ];
 var MENU_HEIGHT = 64;
--- src/widgets/Menu/icons/index.ts
+++ src/widgets/Menu/icons/index.ts
@@ -1,4 +1,5 @@
 export { default as FarmIcon } from "./Farm";
+// export { default as CasinoIcon } from "./Casino";
 export { default as GroupsIcon } from "./Groups";
 export { default as HamburgerIcon } from "./Hamburger";
 export { default as HamburgerCloseIcon } from "./HamburgerClose";
