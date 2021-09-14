53d231952d8868accb3f78ec954d95ae96864924
RuntimeErrorX <kholder2@gmail.com>
2021-09-14 19:03:05 -0400

    update links

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -3902,77 +3902,67 @@ var links = [
 var socials = [
     {
         label: "Twitter",
-        icon: "Twitter",
-        href: "https://twitter.com/pancakeswap",
+        icon: "TwitterIcon",
+        href: "https://twitter.com/capitalvenetian",
     },
     {
         label: "Telegram",
-        icon: "Telegram",
+        icon: "TelegramIcon",
         items: [
             {
                 label: "English",
                 href: "https://t.me/VenetianXCapital",
             },
-            {
-                label: "Bahasa Indonesia",
-                href: "https://t.me/PancakeSwapIndonesia",
-            },
-            {
-                label: "中文",
-                href: "https://t.me/PancakeSwap_CN",
-            },
-            {
-                label: "Tiếng Việt",
-                href: "https://t.me/PancakeSwapVN",
-            },
-            {
-                label: "Italiano",
-                href: "https://t.me/pancakeswap_ita",
-            },
-            {
-                label: "русский",
-                href: "https://t.me/pancakeswap_ru",
-            },
-            {
-                label: "Türkiye",
-                href: "https://t.me/pancakeswapturkiye",
-            },
-            {
-                label: "Português",
-                href: "https://t.me/PancakeSwapPortuguese",
-            },
-            {
-                label: "Español",
-                href: "https://t.me/PancakeswapEs",
-            },
-            {
-                label: "日本語",
-                href: "https://t.me/pancakeswapjp",
-            },
-            {
-                label: "Français",
-                href: "https://t.me/pancakeswapfr",
-            },
-            {
-                label: "Announcements",
-                href: "https://t.me/PancakeSwapAnn",
-            },
-            {
-                label: "Whale Alert",
-                href: "https://t.me/PancakeSwapWhales",
-            },
+            // {
+            //   label: "Bahasa Indonesia",
+            //   href: "https://t.me/PancakeSwapIndonesia",
+            // },
+            // {
+            //   label: "中文",
+            //   href: "https://t.me/PancakeSwap_CN",
+            // },
+            // {
+            //   label: "Tiếng Việt",
+            //   href: "https://t.me/PancakeSwapVN",
+            // },
+            // {
+            //   label: "Italiano",
+            //   href: "https://t.me/pancakeswap_ita",
+            // },
+            // {
+            //   label: "русский",
+            //   href: "https://t.me/pancakeswap_ru",
+            // },
+            // {
+            //   label: "Türkiye",
+            //   href: "https://t.me/pancakeswapturkiye",
+            // },
+            // {
+            //   label: "Português",
+            //   href: "https://t.me/PancakeSwapPortuguese",
+            // },
+            // {
+            //   label: "Español",
+            //   href: "https://t.me/PancakeswapEs",
+            // },
+            // {
+            //   label: "日本語",
+            //   href: "https://t.me/pancakeswapjp",
+            // },
+            // {
+            //   label: "Français",
+            //   href: "https://t.me/pancakeswapfr",
+            // },
+            // {
+            //   label: "Announcements",
+            //   href: "https://t.me/PancakeSwapAnn",
+            // },
+            // {
+            //   label: "Whale Alert",
+            //   href: "https://t.me/PancakeSwapWhales",
+            // },
         ],
     },
-    // {
-    //   label: "Reddit",
-    //   icon: "Reddit",
-    //   href: "https://reddit.com/r/pancakeswap",
-    // },
-    // {
-    //   label: "Instagram",
-    //   icon: "Instagram",
-    //   href: "https://instagram.com/pancakeswap_official",
-    // },
     {
         label: "Github",
         icon: "Github",
